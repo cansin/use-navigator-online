9bb7c5bef4caaeae7c618f29631f25fbcef415be
Cansin Yildiz <cansinyildiz@gmail.com>
2022-05-22 22:24:54 -0700

    1. Start using `npm` instead of `yarn`. 2. Generate `package-lock.json` and run `npm audit fix`.

--- rollup.config.js
+++ rollup.config.js
@@ -2,20 +2,18 @@ import babel from "@rollup/plugin-babel";
 
 import pkg from "./package.json";
 
-export default [
-  {
-    input: "src/index.js",
-    external: ["react"],
-    output: [
-      {
-        globals: { react: "React" },
-        name: "useNavigatorOnline",
-        file: pkg.browser,
-        format: "umd",
-      },
-      { file: pkg.main, format: "cjs" },
-      { file: pkg.module, format: "es" },
-    ],
-    plugins: [babel({ babelHelpers: "bundled" })],
-  },
-];
+export default {
+  input: "src/index.js",
+  external: ["react"],
+  output: [
+    {
+      globals: { react: "React" },
+      name: "useNavigatorOnline",
+      file: pkg.browser,
+      format: "umd",
+    },
+    { file: pkg.main, exports: "default", format: "cjs" },
+    { file: pkg.module, format: "es" },
+  ],
+  plugins: [babel({ babelHelpers: "bundled" })],
+};
